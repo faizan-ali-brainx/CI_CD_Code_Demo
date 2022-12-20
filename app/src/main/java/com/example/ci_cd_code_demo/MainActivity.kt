9e52fd9794f369674ef48b85ce1188b75f654a6e
brainx <faizan.ali@brainxtech.com>
2022-12-20 22:08:01 +0500

    Change 7

--- app/src/main/java/com/example/ci_cd_code_demo/MainActivity.kt
+++ app/src/main/java/com/example/ci_cd_code_demo/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.ci_cd_code_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)))
+        setContentView(R.layout.activity_main)
+        Log.d("", "")
     }
 }
